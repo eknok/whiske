91284d35b05295924e6a21de2703c577c81377de
EikiKanaoka <kakakaeee@gmail.com>
2018-07-28 18:27:41 +0900

    - bundle install dotenv-rails(環境変数読み込み) - Add .env to /.gitignore - Edit index top_controller - Add videochat's base to top/index.html.erb - Create stylesheets/top_index.scss for top/index.html.erb

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,11 @@
 class TopController < ApplicationController
   def index
+    @skywayid = params[:peerid]
+    if @skywayid.nil?
+      @localstream = nil
+    else
+      @localstream = params[:peerid]
+    end
   end
 
   def about
